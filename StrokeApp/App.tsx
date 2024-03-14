5e7165c042e9e20a186f9aca63508fd6b8078562
scottmaclennan <scottmaclennan30@gmail.com>
2024-03-14 04:55:18 +0000

    Added the 4 initial buttons

--- StrokeApp/App.tsx
+++ StrokeApp/App.tsx
@@ -13,55 +13,34 @@ const Separator = () => <View style={styles.separator} />;
 const App = () => (
   <SafeAreaView style={styles.container}>
     <View>
-      <Text style={styles.title}>
-        The title and onPress handler are required. It is recommended to set
-        accessibilityLabel to help make your app usable by everyone.
-      </Text>
       <Button
-        title="Press me"
+        title="Medicine Tracker"
         onPress={() => Alert.alert('Simple Button pressed')}
       />
     </View>
     <Separator />
     <View>
-      <Text style={styles.title}>
-        Adjust the color in a way that looks standard on each platform. On iOS,
-        the color prop controls the color of the text. On Android, the color
-        adjusts the background color of the button.
-      </Text>
       <Button
-        title="Press me"
+        title="Exercise Tracker"
         color="#f194ff"
         onPress={() => Alert.alert('Button with adjusted color pressed')}
       />
     </View>
     <Separator />
     <View>
-      <Text style={styles.title}>
-        All interaction for the component are disabled.
-      </Text>
       <Button
-        title="Press me"
-        disabled
+        title="Games"
+        onPress={() => Alert.alert('Cannot press this one')}
+      />
+    </View>
+    <Separator/>
+    <View>
+      <Button
+        title="Information Hub"
         onPress={() => Alert.alert('Cannot press this one')}
       />
     </View>
     <Separator />
-    <View>
-      <Text style={styles.title}>
-        This layout strategy lets the title define the width of the button.
-      </Text>
-      <View style={styles.fixToText}>
-        <Button
-          title="Left button"
-          onPress={() => Alert.alert('Left button pressed')}
-        />
-        <Button
-          title="Right button"
-          onPress={() => Alert.alert('Right button pressed')}
-        />
-      </View>
-    </View>
   </SafeAreaView>
 );
 
