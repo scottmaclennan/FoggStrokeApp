5ecef405ddef82cb542d973ea7dbeb0279099c4e
scottmaclennan <scottmaclennan30@gmail.com>
2024-03-15 23:46:59 +0000

    Prepped styling for medical tracking

--- StrokeApp/MedScreen.js
+++ StrokeApp/MedScreen.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import { Button, View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, Button, StyleSheet } from 'react-native';
 
-function MedScreen({ navigation }) {
+const MedScreen = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>MedScreen</Text>
+    <View style={styles.container}>
+      <Text>Medicine Details</Text>
+      {/* Details of the medicine */}
       <Button
         title="Go to Home"
         onPress={() => navigation.navigate('Home')}
       />
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default MedScreen;
