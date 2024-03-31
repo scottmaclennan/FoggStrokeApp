950fa146eb636347008636f36dc263069adbedde
scottmaclennan <scottmaclennan30@gmail.com>
2024-03-31 12:23:48 +0100

    about screen created

---
+++ StrokeApp/components/Aboutscreen.js
@@ -0,0 +1,11 @@
+/* components/Aboutscreen.js */
+
+import React, { Component } from "react";
+import { Button, View, Text } from "react-native";
+export default function AboutScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>About Screen</Text>
+    </View>
+  );
+}
