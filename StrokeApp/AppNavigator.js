fdc0e168e63005c578b24e7c61d7c69250ed210f
scottmaclennan <scottmaclennan30@gmail.com>
2024-03-31 03:08:07 +0100

    Replaced app navigator

--- StrokeApp/AppNavigator.js
+++ StrokeApp/AppNavigator.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MedicationsScreen from './screens/MedicationsScreen';
-import AddMedicationScreen from './screens/AddMedicationScreen';
+import HomeScreen from './screens/HomeScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import FeaturesScreen from './screens/FeaturesScreen';
 
 const Tab = createBottomTabNavigator();
 
-function AppNavigator() {
+const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Medications" component={MedicationsScreen} />
-        <Tab.Screen name="Add Medication" component={AddMedicationScreen} />
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Features" component={FeaturesScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default AppNavigator;
